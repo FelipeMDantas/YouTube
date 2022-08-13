74fdd43911c7cdd330fc6601c27077fbf45bb831
FelipeMDantas <felipemdantas@terra.com.br>
2022-08-12 23:19:31 -0300

    Token verification added and Controllers functions incremented.

--- youtube-api/controllers/auth.js
+++ youtube-api/controllers/auth.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync("B4c0//", salt);
+    const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
@@ -25,13 +25,14 @@ export const signin = async (req, res, next) => {
     if (!isCorrect) return next(createError(400, "Wrong credentials."));
 
     const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...otherInfo } = user._doc;
 
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json(user);
+      .json(otherInfo);
   } catch (err) {
     next(err);
   }
